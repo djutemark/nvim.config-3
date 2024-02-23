72eb171b677f03516370595d00d20709a12f9d15
Dennis Jutemark <djutemark@gmail.com>
2024-02-23 07:12:28 +0100

    Add new lsp-overloads keybindings

--- after/plugin/lsp-overloads.lua
+++ after/plugin/lsp-overloads.lua
@@ -3,7 +3,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local bufnr = args.buf
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.server_capabilities.signatureHelpProvider then
-			require('lsp-overloads').setup(client, { })
+			require('lsp-overloads').setup(client, {
+				keymaps = {
+					next_signature = '<C-n>',
+					previous_signature = '<C-m>',
+				},
+			})
 		end
 	end,
 })
