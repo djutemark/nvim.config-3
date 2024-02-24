ba8daeb5ed75fb9ba318c92307165299942f99fc
Dennis Jutemark <>
2024-02-24 21:55:06 +0100

    Add sqlls

--- init.lua
+++ init.lua
@@ -122,7 +122,7 @@ end
 
 local lspconfig = require('lspconfig')
 
-local servers = { 'omnisharp', 'gopls', 'templ', 'svelte', 'tsserver', 'clangd' }
+local servers = { 'omnisharp', 'gopls', 'templ', 'svelte', 'tsserver', 'clangd', 'sqlls' }
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
 		on_attach = on_attach,
