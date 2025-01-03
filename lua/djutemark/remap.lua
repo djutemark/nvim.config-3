c5baab000b1067d96bd16738ae9c6267e6f5a02d
Dennis Jutemark <djutemark@gmail.com>
2025-01-03 09:03:59 +0100

    Remove close window mapping

--- lua/djutemark/remap.lua
+++ lua/djutemark/remap.lua
@@ -23,7 +23,7 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 
 -- Close window
-vim.keymap.set("n", "<leader>qw", [[:q<CR>]])
+-- vim.keymap.set("n", "<leader>qw", [[:q<CR>]])
 
 -- Close buffer
 vim.keymap.set("n", "<leader>qb", ":bp<bar>sp<bar>bn<bar>bd<CR>")
